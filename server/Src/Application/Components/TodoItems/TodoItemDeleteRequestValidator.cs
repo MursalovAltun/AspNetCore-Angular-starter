aabce2da1a08428fe2d232f2a5e4c4b1d26c6d6e
AnsarOrkhan <ansarorkhan@gmail.com>
2020-09-25 22:16:55 +0400

    Added delete feature and Controller for todo

---
+++ server/Src/Application/Components/TodoItems/TodoItemDeleteRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Validators.UserItem;
+using Autofac.Extras.RegistrationAttributes.RegistrationAttributes;
+using EF.Models.Models;
+using FluentValidation;
+
+namespace Application.Components.TodoItems
+{
+    [As(typeof(IValidator<TodoItemDeleteRequest>))]
+    public class TodoItemDeleteRequestValidator : AbstractValidator<TodoItemDeleteRequest>
+    {
+        public TodoItemDeleteRequestValidator(
+            IUserItemPermissionValidator<TodoItem> validator,
+            ITodoItemValidatorService service)
+        {
+            RuleFor(request => request.Id)
+                .MustAsync(async (id, cancellation) =>
+                await service.IsExists(id) && await validator.HasAccess(id));
+        }
+    }
+}
---
+++ server/Src/WebApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Components.TodoItems;
+using Application.Providers.CurrentUserProvider;
+using Application.TodoItems;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    public class TodoItemsController : Controller
+    {
+        private readonly ITodoItemsService _service;
+        private readonly ICurrentUserProvider _currentUserProvider;
+
+        public TodoItemsController(
+            ITodoItemsService service,
+            ICurrentUserProvider currentUserProvider)
+        {
+            _service = service;
+            _currentUserProvider = currentUserProvider;
+        }
+        
+        [HttpGet]
+        public async Task<IEnumerable<TodoItemDto>> Get()
+        {
+            var user = await _currentUserProvider.GetUserAsync();
+            
+            return await _service.GetListAsync(user.Id);
+        }
+
+        [HttpPost]
+        public async Task<TodoItemDto> Post([FromBody] TodoItemAddRequest request)
+        {
+            var user = await _currentUserProvider.GetUserAsync();
+
+            return await _service.AddAsync(request, user.Id);
+        }
+
+        [HttpPut]
+        public async Task<TodoItemDto> Put([FromBody] TodoItemDto request) => await _service.UpdateAsync(request);
+        
+        [HttpDelete]
+        public async Task Delete([FromBody] TodoItemDeleteRequest request) => await _service.DeleteAsync(request);
+    }
+}
